ecf1233440903a9d6e5f4520f8346e9e6f2765e4
gchaudhari <gauravrc07@gmail.com>
2018-12-29 00:53:43 -0600

    data import description

--- part1.R
+++ part1.R
@@ -5,3 +5,26 @@ library(data.table)
 data <-fread("https://github.com/arunsrinivasan/satrdays-workshop/raw/master/flights_2014.csv")
 
 
+##data details###
+
+nrow(data)
+ncol(data)
+
+names(data) ## column names
+
+head(data) ##6 rows data view
+
+str(data) #### one line info about the data 
+
+
+summary(data) ### stats for numerical data
+
+
+data$carrier <- as.factor(data$carrier)  #### summary will change and look better now 
+
+summary(data)
+
+######## Select Keep Columns ##########
+
+
+
